a0a13ef0084bcfe74c22da7934190e6116e84baa
Marcel Zalec <marci.zalec@hotmail.com>
2024-12-18 21:48:49 +0100

    added animations

--- src/app/main/skills/overlay/overlay.component.ts
+++ src/app/main/skills/overlay/overlay.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-overlay',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './overlay.component.html',
   styleUrl: './overlay.component.scss'
 })
