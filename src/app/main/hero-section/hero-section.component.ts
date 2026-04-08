137b26f3a1c58fd3f0a61a5234590ae37e027fe2
Marcel Zalec <marci.zalec@hotmail.com>
2026-04-08 23:40:37 +0200

    add: added scroll prgress bar and fixed some bugs where the curser goes back to default after hover

--- src/app/main/hero-section/hero-section.component.ts
+++ src/app/main/hero-section/hero-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, inject, ViewChild } from '@angular/core';
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { SkillsComponent } from '../skills/skills.component';
 import { ProjectsComponent } from '../projects/projects.component';
@@ -26,13 +26,37 @@ import { DialogComponent } from '../projects/dialog/dialog.component';
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss'
 })
-export class HeroSectionComponent {
+export class HeroSectionComponent implements AfterViewInit {
   github:boolean = false;
   linkedin:boolean = false;
   dialogOpen: boolean = false;
   repeatArray = Array(4)
+  @ViewChild('content') content!: ElementRef;
+  progress = 10;
+  contentHeight = 0;
 
   public dialogService = inject(DialogDataService);
+
+  ngAfterViewInit(): void {
+    const el = this.content.nativeElement;
+    // Gesamthöhe des Inhalts minus sichtbarer Bereich
+    this.contentHeight = el.scrollHeight - window.innerHeight;
+    // Falls kein Scrollen möglich ist → 1 setzen, um NaN zu vermeiden
+    if (this.contentHeight <= 0) {
+      this.contentHeight = 1;
+    }
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
+    // const scrollTop = window.scrollY
+    const doc = document.documentElement;
+
+    const scrollTop = doc.scrollTop || document.body.scrollTop;
+    const scrollHeight = doc.scrollHeight - doc.clientHeight;
+    
+    this.progress = (scrollTop / scrollHeight) * 100;
+  }
 
   hover(id:string) {
     id === 'github' ? (this.github = true): null;
